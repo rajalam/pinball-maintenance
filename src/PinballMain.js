d5b8bee794a375c5d8b41f9c097fddc22e856e8f
rajalam <mikko.rajala@iki.fi>
2024-09-06 17:06:55 +0300

    Change init file to PinballMain, some test input to PinballMain.js and .css

---
+++ src/PinballMain.js
@@ -0,0 +1,14 @@
+import './PinballMain.css';
+
+function PinballMain() {
+
+    return(
+        <div className='background'>
+            <p>Test</p>
+        </div>
+    );
+
+
+}
+
+export default PinballMain;
